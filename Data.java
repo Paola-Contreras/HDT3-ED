e6136b76de7f4884cec6f7db9cb2c40d20012286
Gabriela Paola Contreras Guerra <Contreras@users.noreply.github.com>
2021-02-14 13:38:03 -0600

    Classes

---
+++ Data.java
@@ -0,0 +1,12 @@
+/*******************************************************************************************************************
+    Universidad del Valle de Guatemala 
+    Estructura de Datos 
+    Moises Alonzo 
+    Pablo Emanuel Escobar Sandoval 20936
+    Gabriela Paola Contreras Guerra  20213 
+    Guetemala: 14/02/2021
+********************************************************************************************************************/
+
+public class Data {
+    
+}
