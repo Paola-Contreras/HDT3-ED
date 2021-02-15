036a07cf6fc2c8517fa758a542293dbcf3f6c6f8
Pablo Escobar <77400177+esc20936@users.noreply.github.com>
2021-02-14 22:58:36 -0600

    Código inicial
    Este código genera el archivo txt con los números a ordenar.
    Falta implementar los sorts de manera correcta.

---
+++ Driver.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;   
+public class Driver{
+    public static String generateNumbers(){
+        String result="";
+        Random rand = new Random();
+        int cant = rand.nextInt((3000 - 10) + 1) + 10;
+        for(int i = 0; i<cant; i++){
+            result+=rand.nextInt(9000)+",";
+        }
+        result = result.substring(0,result.length()-1);
+        return result;
+    }
+
+    public static void GenerateFile(){
+        File archivo= new File("Numbers.txt");
+        FileWriter fw;
+        PrintWriter pr;
+        if(!archivo.exists()){
+            try {
+                fw = new FileWriter(archivo,true);
+                pr = new PrintWriter(fw);
+                pr.write(generateNumbers());
+                pr.close();
+                fw.close();
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.out.println(e.getMessage());
+            }
+        }else{
+            try{
+                fw = new FileWriter(archivo,false);
+                pr = new PrintWriter(fw);
+                pr.write(generateNumbers());
+                pr.close();
+                fw.close();
+             }
+             catch(Exception e){
+                 System.out.println(e.getMessage());
+                 
+             }
+        }
+    }
+    public static void main(String[] args){
+        GenerateFile();
+        
+    }
+}
---
+++ Sorts.java
@@ -0,0 +1,58 @@
+public class Sorts{
+    public Comparable[] GnomeSort(Comparable[] arr){
+        int index = 0; 
+  
+        while (index < arr.length) { 
+            if (index == 0) 
+                index++; 
+            if (arr[index].compareTo(arr[index - 1])==1) 
+                index++; 
+            else { 
+                Comparable temp = 0; 
+                temp = arr[index]; 
+                arr[index] = arr[index - 1]; 
+                arr[index - 1] = temp; 
+                index--; 
+            } 
+        }
+        return arr;
+    }
+
+    /*public Comparable[] mergeSort(Comparable[] array){
+        if (array.length < 2) {
+            return;
+        }
+        int mid = array.length / 2;
+        int[] l = new int[mid];
+        int[] r = new int[array.length - mid];
+    
+        for (int i = 0; i < mid; i++) {
+            l[i] = array[i];
+        }
+        for (int i = mid; i < array.length; i++) {
+            r[i - mid] = array[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, array.length - mid);
+        merge(array, l, r, mid, array.length - mid);
+    }
+    public static void merge(Comparable[] a, Comparable[] l, Comparable[] r, int left, int right) {
+ 
+    int i = 0, j = 0, k = 0;
+    while (i < left && j < right) {
+        if (l[i] <= r[j]) {
+            a[k++] = l[i++];
+        }
+        else {
+            a[k++] = r[j++];
+        }
+    }
+    while (i < left) {
+        a[k++] = l[i++];
+    }
+    while (j < right) {
+        a[k++] = r[j++];
+    }
+} */
+
+}
