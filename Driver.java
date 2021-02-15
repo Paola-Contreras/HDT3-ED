f9df20231ab6666299ab76873b7d6b90c739cec8
Pablo Escobar <77400177+esc20936@users.noreply.github.com>
2021-02-15 10:36:11 -0600

    Sorts funcionando
    falta implementar el comparable a GnomeSort y a MergeSort

--- Driver.java
+++ Driver.java
@@ -9,11 +9,10 @@
 ********************************************************************************************************************/
 import java.util.*;
 import java.io.*;   
+import java.util.ArrayList;
 
 public class Driver{
-
-
-    /**
+     /**
      * Random number generation 
      * @return String of numbers 
      */
@@ -29,12 +28,10 @@ public class Driver{
         return result;
     }
 
-
     /**
      * This method creates a new file and writes the random numbers generated previously 
      * @throws Exception If issue occuer
      */
-
     public static void GenerateFile(){
         File archivo= new File("Numbers.txt");
         FileWriter fw;
@@ -64,10 +61,63 @@ public class Driver{
              }
         }
     }
-
-    
+/**
+     * This method format the numbers of the txt file to an ArrayList
+     * @return array of numbers
+     * @throws Exception If issue occuer
+     */
+    public static ArrayList<Integer> getNumbersArray(){
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        try {
+            File myObj = new File("Numbers.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              String[] nums = data.split(",");
+              for(String num : nums){
+                array.add(Integer.parseInt(num));
+              }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return array;
+    }
+/**
+     * This method format an array into a String
+     * @param a arrayList of the numbers
+     * @return the array as string
+     */
+    public static String printArray(ArrayList<Integer> a){
+      String result = "";
+        for(Integer num : a){
+            result += num+", ";
+      }
+      return result;
+    }
     public static void main(String[] args){
         GenerateFile();
         
+        ArrayList<Integer> original = getNumbersArray();
+        Sorts sorts = new Sorts();
+        //Ordenamiento con GnomeSort
+        ArrayList<Integer> gnomeSort =new ArrayList<Integer>(sorts.GnomeSort(original));
+        
+        
+        //Ordenamiento con mergeSort
+        int[] arr = new int[original.size()];
+        for(int i =0 ; i<original.size();i++){
+            arr[i] = original.get(i);
+        }
+        ArrayList<Integer> mergeSort = new ArrayList<Integer>(sorts.sort(arr,0,arr.length-1));
+
+        
+        //Muestra el array ya ordenado
+        System.out.print(printArray(mergeSort));
+
+        
     }
+    
 }
--- Sorts.java
+++ Sorts.java
@@ -7,93 +7,139 @@
     Gabriela Paola Contreras Guerra  20213 
     Guetemala: 14/02/2021
 ********************************************************************************************************************/
-
+import java.util.ArrayList;
 public class Sorts{
+    public Sorts(){
 
-    public Comparable[] GnomeSort(Comparable[] arr){
+    }
+    public ArrayList<Integer> GnomeSort(ArrayList<Integer> array){
+        int n = array.size();
+        int[] arr = new int[array.size()];
+        for(int i =0 ; i<array.size();i++){
+            arr[i] = array.get(i);
+        }
         int index = 0; 
   
-        while (index < arr.length) { 
+        while (index < n) { 
             if (index == 0) 
                 index++; 
-            if (arr[index].compareTo(arr[index - 1])==1) 
+            if (arr[index] >= arr[index - 1]) 
                 index++; 
             else { 
-                Comparable temp = 0; 
+                int temp = 0; 
                 temp = arr[index]; 
                 arr[index] = arr[index - 1]; 
                 arr[index - 1] = temp; 
                 index--; 
             } 
         }
+        
+        for(int i =0 ; i <  array.size();i++){
+            array.set(i,arr[i]);
+        }
+        return array; 
+    }
+
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+ 
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+ 
+        /* Merge the temp arrays */
+ 
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+ 
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
+    // Main function that sorts arr[l..r] using
+    // merge()
+    public ArrayList<Integer> sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m =l+ (r-l)/2;
+ 
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for(int a: arr){
+            nums.add(a);
+        }
+        return nums;
+    }
+
+    public Comparable [] QuickSort (Comparable [] arr){
+
         return arr;
     }
-
-    /*public Comparable[] mergeSort(Comparable[] array){
-        if (array.length < 2) {
-            return;
-        }
-        int mid = array.length / 2;
-        int[] l = new int[mid];
-        int[] r = new int[array.length - mid];
     
-        for (int i = 0; i < mid; i++) {
-            l[i] = array[i];
-        }
-        for (int i = mid; i < array.length; i++) {
-            r[i - mid] = array[i];
-        }
-        mergeSort(l, mid);
-        mergeSort(r, array.length - mid);
-        merge(array, l, r, mid, array.length - mid);
+    public Comparable [] RadixSort (Comparable[] arr){
+    
+        return arr;
     }
-    public static void merge(Comparable[] a, Comparable[] l, Comparable[] r, int left, int right) {
- 
-    int i = 0, j = 0, k = 0;
-    while (i < left && j < right) {
-        if (l[i] <= r[j]) {
-            a[k++] = l[i++];
-        }
-        else {
-            a[k++] = r[j++];
-        }
-    }
-    while (i < left) {
-        a[k++] = l[i++];
-    }
-    while (j < right) {
-        a[k++] = r[j++];
-    }
-} */
-
-public Comparable [] QuickSort (Comparable [] arr){
-
-    return arr;
-}
-
-public Comparable [] RadixSort (Comparable[] arr){
-
-    return arr;
-}
-
-/**
- *  Sort that shows the data using the bubble sort
- * @param arr array that contains the numbers 
- * @return throw back the file data in order
- */
-public Comparable[] BubbleSort(Comparable[]arr){
-    for (int i = 0; i < arr.length; i++){
-        for (int j =0; j< arr.length;j++){
-            if (arr[j].compareTo(arr[i])<0){
-                Comparable temp = arr[i];
-                arr[i]= arr[j];
-                arr[j] = temp;
+    
+    /**
+     *  Sort that shows the data using the bubble sort
+     * @param arr array that contains the numbers 
+     * @return throw back the file data in order
+     */
+    public Comparable[] BubbleSort(Comparable[]arr){
+        for (int i = 0; i < arr.length; i++){
+            for (int j =0; j< arr.length;j++){
+                if (arr[j].compareTo(arr[i])<0){
+                    Comparable temp = arr[i];
+                    arr[i]= arr[j];
+                    arr[j] = temp;
+                }
             }
+    
         }
-
+    
+        return arr; 
     }
-
-    return arr; 
-}
 
 }
